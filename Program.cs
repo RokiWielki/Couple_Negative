f7839c13dbe6edc7fa2e39f8fb464785b762c362
RokiWielki <Cruck222@interia.pl>
2022-11-28 18:44:12 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿//Napisz program, który dla podanej przez użytkownika liczby całkowitej sprawdza, czy
+//jest parzysta oraz czy jest ujemna. 
+
+using System;
+
+namespace Couple_Negative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give a number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your number is: "+Couple(a));
+            Console.WriteLine("Your number is: "+Negative(a));
+        }
+        static string Couple(int a)
+        {
+            if (a == 0)
+            {
+                return "Zero";
+            }
+            else if (a % 2 == 0)
+            {
+                return "Couple";
+            }
+            else
+            {
+                return "Odd";
+            }
+        }
+        static string Negative(int a)
+        {
+            if (a == 0)
+            {
+                return "Zero";
+            }
+            else if (a > 0)
+            {
+                return "Positive";
+            }
+            else
+            {
+                return "Negative";
+            }
+        }
+    }
+}
